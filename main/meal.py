18cad82de6f63fff762caefdd82abb266fbe6ded
cocogenik <smw06075@gmail.com>
2019-03-06 20:24:10 -0500

    remove json write function

--- main/meal.py
+++ main/meal.py
@@ -8,7 +8,6 @@ import datetime
 
 pdf_address = "https://www.nlcsjeju.co.kr/userfiles/nlcsjejumvc/NLCSjeju_menu_3%EC%9B%941%EC%A3%BC%EC%B0%A8.pdf"
 
-write_json()
 ################## PARAMETER CONFIG #########################
 green_start = 1;
 green_end = 10;
